a16cf67178845a7016e9e8fc394b5fa9d9664891
richu abraham <ricabrah@cisco.com>
2022-07-01 14:11:44 +0530

    Add solution to copy list with random pointer

--- linkedlist/copyListWithRandomPointer/copyListWithRandomPointer.go
+++ linkedlist/copyListWithRandomPointer/copyListWithRandomPointer.go
@@ -42,3 +42,25 @@ func copyRandomList(head *Node) *Node {
 	}
 	return newHead
 }
+
+// faster code here
+
+// func copyRandomList(head *Node) *Node {
+// 	if head == nil {
+// 		return nil
+// 	}
+// 	cloneMap := map[*Node]*Node{}
+// 	for cur := head; cur != nil; cur = cur.Next {
+// 		cloneMap[cur] = &Node{}
+// 	}
+// 	for k, v := range cloneMap {
+// 		v.Val = k.Val
+// 		if k.Next != nil {
+// 			v.Next = cloneMap[k.Next]
+// 		}
+// 		if k.Random != nil {
+// 			v.Random = cloneMap[k.Random]
+// 		}
+// 	}
+// 	return cloneMap[head]
+// }
